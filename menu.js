aed1e6cc31b9bdbb5534abe0b6b8ff534c14bd5f
Kaden Neuman <kadenn2019@gmail.com>
2019-10-18 19:01:14 -0700

    finished My part for the most part

--- menu.js
+++ menu.js
@@ -16,7 +16,10 @@ $(document).ready(function(){
                 <div class="title">${app.name}</div>
                 <div class="details">
                     <div class="detail-indv">$${app.price}</div>
-                    <div class="detail-indv">${app.description}</div>
+                    <div class="detail-indv">${app.description}
+                    ${app.extra.spicy?'<i class="fa fa-fire"></i>' : ''}
+                    ${app.extra.glutenfree?'<i class="fa fa-circle"></i>' : ''}
+                    ${app.extra.vegetarian?'<i class="fa fa-leaf"></i>' : ''}</div>
                 </div>
                 `
             }).join('')
@@ -31,7 +34,10 @@ $(document).ready(function(){
                 <div class="title">${ent.name}</div>
                 <div class="details">
                     <div class="detail-indv">$${ent.price}</div>
-                    <div class="detail-indv">${ent.description}</div>
+                    <div class="detail-indv">${ent.description}
+                    ${ent.extra.spicy?'<i class="fa fa-fire"></i>' : ''}
+                    ${ent.extra.glutenfree?'<i class="fa fa-circle"></i>' : ''}
+                    ${ent.extra.vegetarian?'<i class="fa fa-leaf"></i>' : ''}</div>
                 </div>
                 `
             }).join('')
@@ -46,7 +52,10 @@ $(document).ready(function(){
                 <div class="title">${des.name}</div>
                 <div class="details">
                     <div class="detail-indv">$${des.price}</div>
-                    <div class="detail-indv">${des.description}</div>
+                    <div class="detail-indv">${des.description}
+                    ${des.extra.spicy?'<i class="fa fa-fire"></i>' : ''}
+                    ${des.extra.glutenfree?'<i class="fa fa-circle"></i>' : ''}
+                    ${des.extra.vegetarian?'<i class="fa fa-leaf"></i>' : ''}</div>
                 </div>
                 `
             }).join('')
